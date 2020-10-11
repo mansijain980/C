712fed85f7b63097ea147252d3b1fbce8a209e5e
Mansi Jain <63226239+mansijain980@users.noreply.github.com>
2020-10-11 13:28:13 +0530

    Create 215_heapsort.c
    Heap Sort Solution

---
+++ leetcode/src/215_heapsort.c
@@ -0,0 +1,42 @@
+int max(int *a, int n, int i, int j, int k) {
+	int m = i;
+	if (j < n && a[j] > a[m]) {
+		m = j;
+	}
+	if (k < n && a[k] > a[m]) {
+		m = k;
+	}
+	return m;
+}
+
+void downheap(int *a, int n, int i) {
+	while (1) {
+		int j = max(a, n, i, 2 * i + 1, 2 * i + 2);
+		if (j == i) {
+			break;
+		}
+		int t = a[i];
+		a[i] = a[j];
+		a[j] = t;
+		i = j;
+	}
+}
+
+void heapsort(int *a, int n) {
+	int i;
+	for (i = (n - 2) / 2; i >= 0; i--) {
+		downheap(a, n, i);
+	}
+	for (i = 0; i < n; i++) {
+		int t = a[n - i - 1];
+		a[n - i - 1] = a[0];
+		a[0] = t;
+		downheap(a, n - i - 1, 0);
+	}
+}
+
+int findKthLargest(int* nums, int numsSize, int k) {
+	if (numsSize == 0 || k > numsSize) return 0;
+	heapsort(nums, numsSize);
+	return nums[numsSize - k];
+}
